c18453400d469b3036eb39fc10f92a1e108f2fc1
klaas <klaaseebes@gmail.com>
2024-12-04 19:36:27 +0100

    andere config test

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,47 +1,49 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import CrosswordView from '../views/CrosswordView.vue'
+import SlidingView from '../views/SlidingView.vue'
+import MazeView from '../views/MazeView.vue'
+import CatchView from '../views/CatchView.vue'
+import CandyView from '../views/CandyView.vue'
+import MemoryView from '../views/MemoryView.vue'
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: () => import('../views/HomeView.vue')
+      component: HomeView
     },
     {
       path: '/crossword',
       name: 'crossword',
-      component: () => import('../views/CrosswordView.vue')
+      component: CrosswordView
     },
     {
       path: '/sliding',
       name: 'sliding',
-      component: () => import('../views/SlidingView.vue')
-    },
-    {
-      path: '/memory',
-      name: 'memory',
-      component: () => import('../views/MemoryView.vue')
+      component: SlidingView
     },
     {
       path: '/maze',
       name: 'maze',
-      component: () => import('../views/MazeView.vue')
+      component: MazeView
     },
     {
       path: '/catch',
       name: 'catch',
-      component: () => import('../views/CatchView.vue')
+      component: CatchView
     },
     {
       path: '/candy',
       name: 'candy',
-      component: () => import('../views/CandyView.vue')
+      component: CandyView
     },
     {
-      path: '/quiz',
-      name: 'quiz',
-      component: () => import('../views/QuizView.vue')
+      path: '/memory',
+      name: 'memory',
+      component: MemoryView
     }
   ]
 })
