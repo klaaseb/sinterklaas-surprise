0d7692ec233a0bf8e1340287b7ae7ea533337e40
klaas <klaaseebes@gmail.com>
2024-12-04 19:28:57 +0100

    build changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: '/sinterklaas-surprise/',
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
